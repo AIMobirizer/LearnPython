ed219386618ffcb8fcd5e0aef5bd676ebf827894
AIMobirizer <149762998+AIMobirizer@users.noreply.github.com>
2024-08-11 22:30:13 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,10 +10,18 @@ from app.routers import (
 )
 from app.database import engine, SessionLocal
 from app import models, schemas, auth
-
+from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(boards.router)
